779b6b854bf15a015b34833a4af6577da97032b1
Dustin Townsend <dustin.townsend@gmail.com>
2018-04-11 16:33:18 -0400

    added kinvey-flex-sdk type binding file that only declares as module to allow use of import and prevent compiler errors and warnings

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
-/**
- * TODO: add kinvey-flex-sdk type binding file
- * use require instead of import until we have the type binding file
- */
-// import * as sdk from 'kinvey-flex-sdk';
-const sdk = require('kinvey-flex-sdk');
+import * as sdk from 'kinvey-flex-sdk';
 
 import dataHandlers from "./dataHandlers";
 import functionHandlers from "./functionHandlers";
 import authHandlers from "./authHandlers";
 
-sdk.service((err: any, flex: any) => {
+sdk.service((err, flex) => {
     const { data, functions, auth } = flex;
-    
+
     const widgets = data.serviceObject('widgets');
 
     // GET widgets/1
---
+++ types/kinvey-flex-sdk.d.ts
@@ -0,0 +1,1 @@
+declare module "kinvey-flex-sdk";
